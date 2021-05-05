546e3325b1964ee66566f763ba291c71a5baf44d
Aditi <82038181+Aditi21aug@users.noreply.github.com>
2021-05-04 23:19:04 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,38 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var hammer;
+var stone;
+var iron;
+var box;
+
+function setup(){
+    var canvas = createCanvas(1200,600);
+    engine = Engine.create();
+    world = engine.world;
+
+    plane = new Plane(600,height,1200,20)
+    hammer = new Hammer(10,100);
+
+    stone = new Stone(700,350,50,50);
+   
+    iron = new Iron(850,350,50,50);
+
+    box = new Box(900,350,80,80);
+}
+
+function draw(){
+    background("lightBlue");
+    Engine.update(engine);
+
+
+    plane.display();
+    hammer.display();
+    stone.display();
+    iron.display();
+    box.display();
+    
+ 
+}
